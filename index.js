2b67d08e8130bf019095e6dc516a46e0d6225a14
evertoncabral <evertoncab@gmail.com>
2019-08-19 17:17:13 -0300

    atualizacao da soma

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var num = 0;
 
 function add() {
-    num++;
-    mostrar(num);
+    if(num<0){
+        num=0
+        mostrar(num);
+    }else{
+        num++;
+        mostrar(num);
+}
 }
 
 function sub() {
     num--;
-    if(num){
-        num<0;
-        mostrar(num);
+    if(num<0){
+        mostrar(0);
     }else{
-        mostrar("0");
+        mostrar(num);
     }
 }
-    
-
 
 function mostrar(valor) {
     document.getElementById("resultado").innerHTML = valor;
