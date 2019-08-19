5259241ed3849ae4b6be7e0ddda49c8936e13909
evertoncabral <evertoncab@gmail.com>
2019-08-19 16:56:22 -0300

    atualizacao do contador

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+var num = 0;
+
+function add() {
+    num++;
+    mostrar(num);
+}
+
+function sub() {
+    num--;
+    if(num){
+        num<0;
+        mostrar(num);
+    }else{
+        mostrar("0");
+    }
+}
+    
+
+
+function mostrar(valor) {
+    document.getElementById("resultado").innerHTML = valor;
+}
